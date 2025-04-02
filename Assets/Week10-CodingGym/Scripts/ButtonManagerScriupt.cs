d125bb2b11f00224faf5da5a0fec8141512e5579
JoeyLam <joeylam020402@gmail.com>
2025-04-02 18:59:15 -0400

    Week 10

--- Assets/Week10-CodingGym/Scripts/ButtonManagerScriupt.cs
+++ Assets/Week10-CodingGym/Scripts/ButtonManagerScriupt.cs
@@ -7,4 +7,20 @@ public class ButtonManagerScriupt : MonoBehaviour
 {
     public Button buttonSoda;
     public Button buttonMS;
+
+    public PlayerAction script;
+
+    private void Start()
+    {
+        buttonSoda.interactable = false;
+        buttonMS.interactable = false;
+    }
+
+    //private IEnumerator CanAttack()
+    //{
+    //    buttonSoda.interactable = true;
+
+    //  //  yield return StartCoroutine
+        
+    //}
 }
