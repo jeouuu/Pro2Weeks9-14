45442dc5203206f24499a31d7cfb9ccebc3d4f0b
JoeyLam <joeylam020402@gmail.com>
2025-04-05 14:29:27 -0400

    Week11CG 5 - finished.

--- Assets/Week11/Scripts/Knight.cs
+++ Assets/Week11/Scripts/Knight.cs
@@ -11,9 +11,16 @@ public class Knight : MonoBehaviour
     public bool canRun = false;
     public AudioSource audioSource;
 
+    Vector2 currentPos;
+    Vector2 newPos;
+    bool moving = false;
+    public LineRenderer lr;
+    public Vector2[] listOfPoints = new Vector2[2];
+
     private void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        lr.positionCount = 0;
     }
 
     private void Update()
@@ -24,45 +31,87 @@ public class Knight : MonoBehaviour
             canRun = false;
         }
 
+
+        currentPos = transform.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+        } else
+        {
+            moving = false;
+        }
+
         if (canRun)
         {
-            Move();
-        } 
+            Move(currentPos, newPos);
+        }
+
+        listOfPoints[0] = sr.transform.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            listOfPoints[1] = newPos;
+            lr.positionCount++;
+            lr.SetPosition(lr.positionCount - 1, newPos);
+        }
+
+        if ((Vector2)transform.position == newPos)
+        {
+            listOfPoints[0] = transform.position;
+            lr.positionCount = 0;
+        }
 
     }
 
-    private void Move()
+    private void Move(Vector2 currentPos, Vector2 newPos)
     {
-        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime ,0,0);
-        if(Input.GetAxis("Horizontal") < 0)
-        {
-            sr.flipX = true;
-        }
-        else if(Input.GetAxis("Horizontal") > 0)
-        {
-            sr.flipX = false;
-        }
 
-        if(Input.GetAxis("Horizontal") != 0)
+        Vector2 direction = newPos - currentPos;
+        direction.Normalize();
+
+        currentPos += direction * speed * Time.deltaTime;
+        transform.position = currentPos;
+
+        if (moving)
         {
-            animator.SetBool("isRunning",true);
+            animator.SetBool("isRunning", true);
         } else
         {
             animator.SetBool("isRunning", false);
         }
+
+
+
+
+        //use input to move the player
+
+        //transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime ,0,0);
+        //if(Input.GetAxis("Horizontal") < 0)
+        //{
+        //    sr.flipX = true;
+        //}
+        //else if(Input.GetAxis("Horizontal") > 0)
+        //{
+        //    sr.flipX = false;
+        //}
+
+        //if(Input.GetAxis("Horizontal") != 0)
+        //{
+        //    animator.SetBool("isRunning",true);
+        //} else
+        //{
+        //    animator.SetBool("isRunning", false);
+        //}
     }
 
-    private void Attack()
-    {
-       
-    }
 
     public void AttackHasFinished()
     {
         canRun = true;
     }
 
-    public void FootStepA()
+    public void FootStep()
     {
         audioSource.Play();
 
