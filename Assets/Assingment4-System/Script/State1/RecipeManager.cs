b31b73036378922cae806c32b8259d3353f26e24
JoeyLam <joeylam020402@gmail.com>
2025-04-08 22:07:38 -0400

    Cleanded up the codes and files.

--- Assets/Assingment4-System/Script/State1/RecipeManager.cs
+++ Assets/Assingment4-System/Script/State1/RecipeManager.cs
@@ -13,68 +13,14 @@ public class RecipeManager : MonoBehaviour
     //var for the countdown timer
     public float waitTime = 1;
 
-    //ref of the UI elements
-    public Image cookingImage1;
-    public Image foodImage1;
-    public TextMeshProUGUI numOfFood1; 
-    public Image cookingImage2;
-    public Image foodImage2;
-    public TextMeshProUGUI numOfFood2;
-    public Image cookingImage3;
-    public Image foodImage3;
-    public TextMeshProUGUI numOfFood3;
-
-    //ref of the sprites
-    public Sprite[] cookingSprites;
-    public Sprite[] foodSprites;
-
-    private bool canChooseCooking = true;
-    private bool canChooseFood = true;
-    private bool canChooseNumOfFood = true;
-
     private void Update()
     {
         //double check if gameState is at 1 and if the recipe canvas is on. If yes then continue on. 
         if(gameStateManager.gameState == 1 && recipeCanvas.activeSelf)
-        {
-            //if (canChooseCooking && canChooseFood && canChooseNumOfFood)
-            //{
-            //    GenerateRecipeCooking();
-            //    GenerateRecipeFood();
-            //    GenerateRecipeNumOfFood();
-            //}
-            
+        {            
             StartCoroutine(RecipeCountDown());
         }
     }
-
-    private void GenerateRecipeCooking()
-    {
-        //pick a random cooking method then set the can choose cookking to false(so it only pick once)
-        cookingImage1.sprite = cookingSprites[Random.Range(0, cookingSprites.Length)];
-        cookingImage2.sprite = cookingSprites[Random.Range(0, cookingSprites.Length)];
-        cookingImage3.sprite = cookingSprites[Random.Range(0, cookingSprites.Length)];
-        canChooseCooking = false;
-    }
-
-    private void GenerateRecipeFood()
-    {
-        //pick a random food, set can choose to false
-        foodImage1.sprite = foodSprites[Random.Range(0, foodSprites.Length)];
-        foodImage2.sprite = foodSprites[Random.Range(0, foodSprites.Length)];
-        foodImage3.sprite = foodSprites[Random.Range(0, foodSprites.Length)];
-        canChooseFood = false;
-    }
-
-    private void GenerateRecipeNumOfFood()
-    {
-        //pick a random number of food, set can choose to false
-        numOfFood1.text = Random.Range(1, 5).ToString();
-        numOfFood2.text = Random.Range(1, 5).ToString();
-        numOfFood3.text = Random.Range(1, 5).ToString();
-        canChooseNumOfFood = false;
-    }
-
     private IEnumerator RecipeCountDown()
     {
         //count down timer for showing the reecipe. 
